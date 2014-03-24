bca38e1411bfa1468997122f5cba902828510cd3
MizaelGalvez <mizaelgalvez@gmail.com>
2014-03-24 02:39:45 -0700

    cambiando la manera de agregar modulos, y quitando el admin de contenido, se creara el modulo completo

--- plataforma/controladores/administrador.js
+++ plataforma/controladores/administrador.js
@@ -1,6 +1,3 @@
-var _ = require('underscore');
-
-
 var Controladoradministrador = function (server) {
 	
 	var users = [];
@@ -28,15 +25,6 @@ var Controladoradministrador = function (server) {
 			modulotitulo= "Modulo 1",
 			modulodescrip = "Aprenderemos la diferencia entre energía solar fotovoltaica y energía solar térmica, así como los fundamentos para usar la energía solar fotovoltaica de la manera más eficiente posible."
 		res.render('adminmodulos', (modulotitulo, modulodescrip));
-	});
-//
-//*********************************************************************
-//
-	server.get('/admincontenido', NoEstaLogeado, function (req, res) {
-			modulotitulo= "Modulo 1",
-			modulotexto= "este es el contenido del curso, se simula que ay mucho texto o almenos algo de explicasion he de igual manera se regresara el valor de un video para poder renderizarlo saludos",
-			modulovideo= "videos/youtube.MOV",
-		res.render('admincontenido', (modulotitulo, modulotexto));
 	});
 
 };
--- plataforma/modelos/modulos.js
+++ plataforma/modelos/modulos.js
@@ -4,6 +4,21 @@ var models = require('./modelos'),
 var modulosSchema = Schema({
 	Titulo : 'string',
 	Descripcion : 'string',
+	Contenido : 'string',
+	videotitulo1: 'string',
+	url1 : 'string',
+	videotitulo2: 'string',
+	url2 : 'string',
+	videotitulo3: 'string',
+	url3 : 'string',
+	videotitulo4: 'string',
+	url4 : 'string',
+	videotitulo5: 'string',
+	url5 : 'string',
+	videotitulo6: 'string',
+	url6 : 'string',
+	videotitulo7: 'string',
+	url7 : 'string',
 });
 
 var Modulos = models.model('cursos', modulosSchema);
